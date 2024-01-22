49b47b01e44b6153c39baace696aa49a9574dabb
kevin kaneza <65622766+kevindermot99@users.noreply.github.com>
2024-01-22 17:13:31 +0200

    settings modal in progress

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,10 +3,15 @@ import { Link, useNavigate } from 'react-router-dom'
 import { FaSearch } from "react-icons/fa";
 import { GrClose } from "react-icons/gr";
 import axios from 'axios'
+import { HiOutlineCog6Tooth } from "react-icons/hi2";
+import { IoClose } from "react-icons/io5";
+import { FaCheckCircle } from "react-icons/fa";
+
 
 function Navbar() {
     const [loggingOut, setLoggingOut] = useState(false)
     const [openAccountMenu, setOpenAccountMenu] = useState(false)
+    const [openSettings, setOpenSettings] = useState(false)
     const navigate = useNavigate()
 
     // auth the page
@@ -53,6 +58,19 @@ function Navbar() {
 
     }
 
+    const settings = () => {
+        setOpenAccountMenu(false)
+        setOpenSettings(true)
+    }
+
+    const closeSettings = () => {
+        setOpenSettings(false)
+    }
+
+    const updateUsename = (e) => {
+        e.preventDefault()
+    }
+
     return (
         <div className='navbar'>
             <ul>
@@ -75,7 +93,7 @@ function Navbar() {
                         <>
                             <p className={`account-letter ${openAccountMenu ? "open-modal" : ''}`} onClick={toggleModal}>{userNames.charAt(0)}</p>
                             <div className='account-menu'>
-                                <h4>{userNames}</h4>
+                                <div className='am-header'><h4>{userNames}</h4><div className="settings"></div><HiOutlineCog6Tooth onClick={settings} /></div>
                                 <p>{userEmail}</p>
                                 <button onClick={logout}>
                                     {loggingOut ? <span className='btnloader'></span> : <span>Log Out</span>}
@@ -95,6 +113,41 @@ function Navbar() {
 
                 </div>
             </ul>
+
+            {/* settings modal */}
+            <div className={`settings-wrapper ${openSettings ? 'show' : ''}`}>
+                <div className="settings-body">
+                <div className='settings'>
+            <div className='s-header'>
+                <h1>Account Settings</h1>
+                <IoClose onClick={closeSettings} />
+            </div>
+            <div className="s-section">
+                <h3> Change username </h3>
+                <p>Enter your desired display name below:</p>
+                <form onSubmit={updateUsename}>
+                    <input type="text" className='edit' value={userNames} />
+                    <input type="submit" value="Save" className='save' />
+                </form>
+            </div>
+            <div className="s-section">
+                <h3> Theme Settings </h3>
+                <p>Choose your preferred theme:</p>
+                <div className="themes">
+                    <div>
+                        <div className="theme-box active"><FaCheckCircle /></div>
+                        <p>Light</p>
+                    </div>
+                    <div>
+                        <div className="theme-box dark"><FaCheckCircle /></div>
+                        <p>Dark</p>
+                    </div>
+                </div>
+            </div>
+
+        </div >
+                </div>
+            </div>
         </div>
     )
 }
